fac6fc9a3e1e4dd9c03dfdc1fd39d78d95df1c45
Borodin Vadim <vbo@vbo.name>
2013-01-30 00:37:02 +0400

    map browser reimplemented using raphael.panzoom and jquery.mousewheel

--- htdocs/static/js/setup.js
+++ htdocs/static/js/setup.js
@@ -2,32 +2,34 @@ $(document).ready(function () {
     // TODO: remove overscroll.js. do the photoshop-like scrolling
     // and zooming by hand via paper.setViewBox()
     // See http://jsfiddle.net/9zu4U/10/
-    var overscroll = false;
-    $(document).bind("keydown", "space", function (evt) {
-        if (!overscroll) {
-            overscroll = true;
-            var opts = {showThumbs: false, wheelDelta: 60, dragHold: true};
-            $('.map_holder').overscroll(opts);
-        }
-    });
-    $(document).bind("keyup", "space", function (evt) {
-        overscroll = false;
-        $('.map_holder').removeOverscroll();
-    });
-    var on_resize = function (left, top) {
-        var opts = {showThumbs: false, wheelDelta: 60, dragHold: true};
-        if (left > 0) {
-            opts.scrollLeft = left;
-        }
-        if (top > 0) {
-            opts.scrollTop = top;
-        }
-        $('.map_holder').width($(window).width());
-        $('.map_holder').height($(window).height());
-        $('.map_holder').overscroll(opts);
-        $('.map_holder').removeOverscroll();
+    //var overscroll = false;
+    //$(document).bind("keydown", "space", function (evt) {
+    //    if (!overscroll) {
+    //        overscroll = true;
+    //        var opts = {showThumbs: false, wheelDelta: 60, dragHold: true};
+    //        $('.map_holder').overscroll(opts);
+    //    }
+    //});
+    //$(document).bind("keyup", "space", function (evt) {
+    //    overscroll = false;
+    //    $('.map_holder').removeOverscroll();
+    //});
+    var on_resize = function () {
+        //var opts = {showThumbs: false, wheelDelta: 60, dragHold: true};
+        //if (left > 0) {
+        //    opts.scrollLeft = left;
+        //}
+        //if (top > 0) {
+        //    opts.scrollTop = top;
+        //}
+        var w = $(window).width(), h = $(window).height();
+        $('.map_holder').width(w);
+        $('.map_holder').height(h);
+        map.paper.setSize(w, h);
+        //$('.map_holder').overscroll(opts);
+        //$('.map_holder').removeOverscroll();
     };
     $(window).resize(on_resize);
-    on_resize(19980, 19940);
+    on_resize();
 });
 
